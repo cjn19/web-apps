9684bc644632387b50510d3d0319fbd4e513706a
Jayanth  Challapalli <jayanth.challapalli@kellogg.northwestern.edu>
2024-05-03 21:32:03 +0000

    FINAL

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
+  resources "tacos"
+  resources "dice"
+  resources "poker"
+  resources "companies"
+  resources "contacts"
 end
